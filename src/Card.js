8eb858fad72f7b72dc9244966e6933e288901c65
tkdrb12 <tkdrb15@gmail.com>
2023-12-28 21:40:13 +0900

    feat: 카드 객체 생성 기능 추가

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,31 @@
 class Card {
+  #isWin;
+  #$node;
+  #cards;
+
+  constructor(isWin, cards) {
+    this.#isWin = isWin;
+    this.#cards = cards;
+
+    this.#createNode();
+  }
+
+  #createNode() {
+    const $node = document.createElement('button');
+    $node.classList.add('card');
+    $node.innerText = '카드입니다';
+
+    this.#$node = $node;
+  }
+
   // 필드 값은
   constructor() {}
+
+  render($parent) {
+    if ($parent) {
+      $parent.appendChild(this.#$node);
+    }
+  }
 }
 
 export default Card;
