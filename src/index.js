155a22f6aadc120a701917bb234879f64d878f21
gykwak03 <110815273+gykwak03@users.noreply.github.com>
2024-01-07 08:28:38 +0900

    {기능은 다 구현된 거 같은데 테스트 통과가 안돼요ㅜㅠ}

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
 import '../index.css';
 
-// 실제 실행 코드 작성
-// ex)cardGame.start()
+let cardsContainer = document.querySelector('#cards');
+class Card {
+  card;
+  id;
+
+  constructor(id) {
+    let card = document.createElement('button'); //
+    card.classList.add('card'); //
+    card.textContent = '카드입니다';
+
+    this.card = card;
+    this.id = id;
+
+    this.render();
+  }
+
+  render() {
+    cardsContainer.appendChild(this.card); //부모 노드와 자식 노드 연결 
+  }
+}
+
+const card1 = new Card('id1');
+const card2 = new Card('id2');
+const card3 = new Card('id3');
+
+const win = Math.floor(Math.random() * 3);
+//buttons가 list가 됨
+const buttons = document.querySelectorAll('button');
+
+for (let i = 0; i<3; i++){
+  buttons[i].addEventListener('click', () => {
+    if(win == i){
+      document.write('정답입니다')
+    } else{
+      document.write('꽝입니다')
+    }
+  });
+}
