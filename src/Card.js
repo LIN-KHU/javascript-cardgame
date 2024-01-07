460f94a27b5b60baf7aa05129dcffb8ca949e7ee
gykwak03 <110815273+gykwak03@users.noreply.github.com>
2024-01-08 03:15:09 +0900

    {class 분리하지 않은 commit입니다.}

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,17 @@
 class Card {
   // 필드 값은
-  constructor() {}
+  constructor() {
+    let card = document.createElement("button"); //
+    card.classList.add("card"); //
+    card.textContent = "카드입니다";
+
+    this.card = card;
+    this.render();
+  }
+
+  render() {
+    cardsContainer.appendChild(this.card); //부모 노드와 자식 노드 연결
+  }
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
-import '../index.css';
+import "../index.css";
 
-let cardsContainer = document.querySelector('#cards');
 class Card {
-  card;
-  id;
-
-  constructor(id) {
-    let card = document.createElement('button'); //
-    card.classList.add('card'); //
-    card.textContent = '카드입니다';
+  constructor() {
+    let card = document.createElement("button"); //
+    card.classList.add("card"); //
+    card.textContent = "카드입니다";
 
     this.card = card;
-    this.id = id;
-
     this.render();
   }
 
   render() {
-    cardsContainer.appendChild(this.card); //부모 노드와 자식 노드 연결 
+    cardsContainer.appendChild(this.card); //부모 노드와 자식 노드 연결
   }
 }
 
-const card1 = new Card('id1');
-const card2 = new Card('id2');
-const card3 = new Card('id3');
+let cardsContainer = document.querySelector("#cards");
 
-const win = Math.floor(Math.random() * 3);
+const card1 = new Card();
+const card2 = new Card();
+const card3 = new Card();
+
+const randomNum = Math.floor(Math.random() * 3);
 //buttons가 list가 됨
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
-for (let i = 0; i<3; i++){
-  buttons[i].addEventListener('click', () => {
-    if(win == i){
-      document.write('정답입니다')
-    } else{
-      document.write('꽝입니다')
+cardsContainer.appendChild(card1.card);
+cardsContainer.appendChild(card2.card);
+cardsContainer.appendChild(card3.card);
+
+const numbers = [0, 1, 2];
+numbers.forEach(function (number) {
+  buttons[number].addEventListener("click", () => {
+    if (randomNum == number) {
+      document.write("정답입니다");
+    } else {
+      document.write("꽝입니다");
     }
   });
-}
+});
