e7b5510062caf8df6bca7a49120e348cb10002af
min <kmss0206@gmail.com>
2024-01-09 16:26:34 +0900

    수정입니다.

--- src/index.js
+++ src/index.js
@@ -5,36 +5,57 @@ import '../index.css';
 
 class Card {
   constructor() {
-    const element = document.createElement('button');
-    element.className = 'card';
-    element.innerText = '카드입니다.';
-
-    this.element = element;
-    element.addEventListener('click', this.handleClick);
-
-    const cards = document.querySelector('#cards');
-    cards.appendChild(element);
-
-    const lose = Math.random() < 0.33;
-    if (lose) {
-        this.element.dataset.result = 'loser';
-    }
+    this.createCard();
+    this.addEventHandler();
+    this.addCard();
   }
 
-
-
-  
-  handleClick() {
-    if (this.dataset.result == 'loser'){
-        alert("꽝 입니다!")
-    }
-    else{
-        alert("당첨 입니다.")
-    }    
-  }
-  
+ createCard(){
+  this.element = document.createElement('button');
+  this.element.className = 'card';
+  this.element.innerText = '카드입니다.';
 }
 
-const card1 = new Card();
-const card2 = new Card();
-const card3 = new Card();
+ addEventHandler(){
+  this.element.addEventListener('click', this.handleClick.bind(this));
+ }
+
+addCard(){
+  const cards = document.querySelector('#cards');
+  cards.appendChild(this.element);
+}
+
+
+cardShuffle(){
+  const card1 = new Card();
+  const card2 = new Card();
+  const card3 = new Card();
+
+  card1.element.dataset.result = 'loser';
+  card2.element.dataset.result = 'winner';
+  card3.element.dataset.result = 'winner';
+
+
+  const cardList = [card1, card2, card3]
+  cardList.sort(() => Math.random() - 0.5);
+
+  const cardsContainer = document.querySelector('#cards');
+  cardsContainer.innerHTML = ''; 
+
+  cardList.forEach(card => card.addCard());
+}
+
+
+handleClick() {
+  if (this.element.dataset.result == 'loser'){
+      this.element.innerText = '꽝 입니다.';
+  }
+  else{
+    this.element.innerText = '당첨 입니다.';
+  }    
+ }
+}
+
+
+const cardGame = new Card();
+cardGame.cardShuffle();
