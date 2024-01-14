45e5447c93688d9b49376cbe573fe6ca9a76dd95
sujin0707 <soojin2650@khu.ac.kr>
2024-01-14 18:06:11 +0900

    ifWinner, ifLoser, checkResult, restartGame 함수 추가

--- src/CardGame.js
+++ src/CardGame.js
@@ -4,17 +4,19 @@ class CardGame {
   constructor() {
     this.cardContainer = document.querySelector("#cards");
     this.resultContainer = document.querySelector("#result");
-    this.cards = this.createCards();
+    this.cards = this.createCards(this);
     this.renderCards();
+    this.remainAttempts = 2;
+    this.isGameOver = false;
   }
 
-  createCards() {
+  createCards(game) {
     const cards = [];
     const winnerLocation = Math.floor(Math.random() * 3);   
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
       const isWinner = i === winnerLocation;
-      const card = new Card(isWinner, this.resultContainer);
+      const card = new Card(isWinner, game);
       cards.push(card);
     }
     return cards;
@@ -24,6 +26,48 @@ class CardGame {
     this.cards.forEach((card) => {
       this.cardContainer.appendChild(card.element);
     });
+  }
+
+  ifWinner() {
+    this.isGameOver = true;
+    this.checkResult();
+  }
+
+  ifLoser() {
+    if (this.remainAttempts > 0) {
+      this.remainAttempts--;
+      this.resultContainer.textContent = '남은횟수 : ' + this.remainAttempts;
+    } else {
+      this.resultContainer.textContent = '[실패!] 게임이 종료되었습니다.';
+      this.isGameOver = true;
+      this.checkResult();
+    }
+  }
+
+  checkResult() {
+    const existingButton = this.resultContainer.querySelector('button');
+    if (existingButton) {
+      existingButton.remove();
+    }
+    
+    if (this.isGameOver) {
+      const restartButton = document.createElement('button');
+      restartButton.textContent = '재시작';
+      restartButton.addEventListener('click', () => this.restartGame());
+      this.resultContainer.appendChild(restartButton);
+    }
+  }
+
+  restartGame() {
+    this.isGameOver = false;
+    this.resultContainer.innerHTML = '';
+    this.remainingAttempts = 2;
+    this.cards.forEach((card) => {
+      card.element.removeEventListener('click', card.handleCardClick);
+    });
+    this.cardContainer.innerHTML = '';
+    this.cards = this.createCards();
+    this.renderCards();
   }
 
   start() {
